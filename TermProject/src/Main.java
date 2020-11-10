a185214190ce013241030edcc7a4f5df99a2907e
imjeongwoo <limjeong1219@gmail.com>
2020-11-10 19:23:22 +0900

    1차 제출

--- TermProject/src/Main.java
+++ TermProject/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Random rand = new Random();
 
         int n = sc.nextInt();
-        int[] arr = new int[n+1];
+        int[] arr = new int[n+1]; // 0번째 index 사용x
         int start, end, min, max, sum;
 
         for (int i = 1; i <= n; i++) {
@@ -16,18 +16,19 @@ public class Main {
         }
         System.out.println();
 
-        int[] range = new int[2];
-            for (int i = 0; i < 2; i++) {
-                range[i] = sc.nextInt(); // 구간 입력
-            }
-            start = range[0]; end = range[1];
-            max = min = arr[start];
-            sum = 0;
-            for (int i = start; i <= end; i++) {
-                if(arr[i] < min) min = arr[i];
-                if(arr[i] > max) max = arr[i];
-                sum += arr[i];
-            }
-            System.out.println(min + " " + max + " " + sum);
+        int[] range = new int[2]; // 구간을 담는 배열
+        for (int i = 0; i < 2; i++) {
+            range[i] = sc.nextInt(); // 구간 입력
+        }
+
+        start = range[0]; end = range[1]; // 구간 시작, 끝 설정
+        max = min = arr[start];
+        sum = 0;
+        for (int i = start; i <= end; i++) {
+            if(arr[i] < min) min = arr[i];
+            if(arr[i] > max) max = arr[i];
+            sum += arr[i];
+        }
+        System.out.println(min + " " + max + " " + sum);
     }
 }
