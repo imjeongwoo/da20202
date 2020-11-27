f9eb649b0b5d50fdde96180c75cde23c935552dd
imjeongwoo <limjeong1219@gmail.com>
2020-11-27 15:58:59 +0900

    2차제출

--- TermProject/src/TimeTest.java
+++ TermProject/src/TimeTest.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 public class TimeTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Random rand = new Random();
+        Random rand = new Random(100);
 
-        int n = sc.nextInt();  // n의 값 입
+        int n = sc.nextInt();  // n의 값 입력
         int[] arr = new int[n+1];
         int min, max, sum;
 
         for (int i = 1; i <= n; i++) {
-            arr[i] = rand.nextInt(100) + 1; //  data의 범위 1 ~ 100
+            arr[i] = rand.nextInt(1000000) + 1; //  data의 범위 1 ~ 100000
 //            System.out.print(arr[i]+" ");   // data 값 출력
         }
 //        System.out.println();
@@ -20,10 +20,14 @@ public class TimeTest {
 
         long total_time = 0;
         for (int j = 0; j < k; j++) {
-            int maxStart = n-1, minSatrt =1, maxEnd = n;
-            int start = (int)(Math.random() * (maxStart - minSatrt + 1) + minSatrt);    // start의 범위 1 ~ n-1
-            int minEnd = start+1;
-            int end = (int)(Math.random() * (maxEnd - minEnd + 1) + minEnd);    // end의 범위 start+1 ~ n
+            int start = rand.nextInt(n) + 1;
+            int end = rand.nextInt(n) + 1;
+
+            if (start > end) {
+                int tmp = start;
+                start = end;
+                end = tmp;
+            }
 
             max = min = arr[start];
             sum = 0;
